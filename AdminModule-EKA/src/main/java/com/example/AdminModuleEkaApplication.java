8c95f9a46951212b552ce8895bd9fc88281fa638
bharathkumar-rjay <bharath.k@rjaytechnologies.com>
2024-06-24 14:58:07 +0530

    hard code added

--- AdminModule-EKA/src/main/java/com/example/AdminModuleEkaApplication.java
+++ AdminModule-EKA/src/main/java/com/example/AdminModuleEkaApplication.java
@@ -1,17 +1,38 @@
 package com.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.example.entity.User;
+import com.example.service.UserService;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 public class AdminModuleEkaApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(AdminModuleEkaApplication.class, args);
 	}
-
+	@Bean
+    public CommandLineRunner createAdminUser(UserService userService, PasswordEncoder passwordEncoder) {
+        return args -> {
+            String adminEmail = "admin@example.com";
+            String adminPassword = "1233";
+            String adminRole = "Admin";
+ 
+            if (userService.findByEmailAndRole(adminEmail,adminRole) == null) {
+                User adminUser = new User();
+                adminUser.setEmail(adminEmail);
+                adminUser.setPassword(passwordEncoder.encode(adminPassword));
+                adminUser.setRole(adminRole);
+                userService.savedata(adminUser);
+            }
+        };}
 }
